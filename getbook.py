617ae602371753918f03e04802ab1ce2b3382688
Mihas' Varantsou <meequz@gmail.com>
2015-05-25 00:10:51 +0300

    completely rewrite with lxml

--- getbook.py
+++ getbook.py
@@ -1,89 +1,48 @@
 #! /usr/bin/env python3
 # coding: utf-8
-import urllib.request
 import sys
 
-def del_tags(string):
-	res = ''
-	skip_flag = 0
-	for char in string:
-		if char == '>':
-			skip_flag = 0
-			continue
-		if skip_flag:
-			continue
-		else:
-			if char == '<':
-				skip_flag = 1
-				continue
-		res += char
-	return res
-def write_to_file(what_add, target_file, end='\n'):
-	outfile = open(target_file, 'w')
-	outfile.write( str(what_add) + end )
-	outfile.close()
-def get_page_content(url):
-	page = urllib.request.urlopen(url)
-	return page.read().decode(encoding='UTF-8')
-def get_refs(page):
-	res = []
-	st = page.find('<a name="n_1"></a>')
-	if st != -1:
-		cn = 1
-		while True:
-			st = page[st:].find('<p class=') + len(page[:st])
-			if st != -1:
-				fn = page[st:].find('</p>') + len(page[:st])
-				reftext = del_tags(page[st:fn])
-				if reftext:
-					res.append(reftext)
-					cn += 1
-					st += len(reftext) + 8
-				else:
-					break
-			else:
-				break
-	return res
-def cut_start(book):
-	start_sign = '[Ю] [Я]'
-	book = book[book.find(start_sign):]
-	bookst = book.find('\n')
-	return book[bookst:]
-def del_empty_lines(sometext):
-	work = sometext.split('\n')
-	res = []
-	skipflag = 0
-	for idx, line in enumerate(work):
-		if skipflag:
-			skipflag = 0
-			continue
-		if line:
-			res.append(line)
-			continue
-		try:
-			if not work[idx+1]:
-				res.append(line)
-				skipflag = 1
-				continue
-		except IndexError:
-			break
-	return '\n'.join(res)
-def strip_lines(sometext):
-	work = sometext.split('\n')
-	res = [line.strip() for line in work]
-	return '\n'.join(res)
+import requests
+import lxml.html
+from lxml.cssselect import CSSSelector
 
-page = get_page_content(sys.argv[1])
-title = sys.argv[2]
-print('Page received ({} characters), processing...'.format(len(page)))
 
-page = page.replace('<p class=', '\n<p class=')
-for idx, ref in enumerate(get_refs(page)): page = page.replace('['+str(idx+1)+']', '['+ref+']')
+# get page
+url = sys.argv[1]
+page = requests.get(url).text
+page = page.replace('\xa0', ' ')
+tree = lxml.html.fromstring(page)
 
-book = del_tags(page)
-book = cut_start(book)
-book = strip_lines(book)
-book = del_empty_lines(book)
 
-write_to_file(book, target_file=title+'.txt', end='\n')
-print('Done:', title+'.txt')
+# get title
+title_tag = CSSSelector('div#main h1')(tree)[0]
+title = title_tag.text_content()
+fb2 = title.find(' (fb2)')
+if fb2 != -1:
+    title = title[:fb2]
+
+
+# get text
+text_tag = CSSSelector('div#main div._ga1_on_')(tree)[0]
+text = text_tag.text_content().strip()
+
+
+# get refs
+ref_sup_tags = CSSSelector('sup')(text_tag)
+ref_tags = [CSSSelector('a')(ref_sup_tag)[1] for ref_sup_tag in ref_sup_tags]
+refs = [ref_tag.get('title').strip() for ref_tag in ref_tags]
+
+
+# fill the refs
+template = '[{}]'
+i = 1
+ref_marker = template.format(i)
+while text.find(ref_marker) != -1:
+    text = text.replace(ref_marker, template.format(refs[i-1]))
+    i += 1
+    ref_marker = template.format(i)
+
+
+# write to file
+with open (title+'.txt', 'w') as f:
+    f.write(text)
